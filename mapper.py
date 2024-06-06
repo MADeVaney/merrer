c2ea06a1966177e4b3cefe10e24717c22a681808
Miles DeVaney <89801199+MADeVaney@users.noreply.github.com>
2024-06-05 21:16:40 -0700

    Add files via upload

--- mapper.py
+++ mapper.py
@@ -10,13 +10,11 @@ import sys
 
 for line in sys.stdin:
     line = line.strip()
-    words = []
     word = ''
     for char in line:
         if char.isalpha():
             word += char
         elif word != '':
-            words.append(word.lower())
+            print('%s\t%s' % (word, 1))
             word = ''
-    for word in words:
-        print('%s\t%s' % (word, 1))
+        
