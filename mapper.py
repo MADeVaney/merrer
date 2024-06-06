18ba65a9420858203e1c8f62641802378b8b846f
Miles DeVaney <milesdevaney@charter.net>
2024-06-05 20:32:29 -0700

    First attempt

--- mapper.py
+++ mapper.py
@@ -2,8 +2,22 @@
 # -*-coding:utf-8 -*
 import sys 
   
-for line in sys.stdin: 
+'''for line in sys.stdin: 
     line = line.strip() 
     words = line.split() 
+    for word in words:
+        print('%s\t%s' % (word, 1))'''
+
+
+for line in sys.stdin:
+    line = line.strip()
+    words = []
+    word = ''
+    for char in line:
+        if char.isalpha():
+            word += char
+        else:
+            words.append(word.lower())
+            word = ''
     for word in words:
         print('%s\t%s' % (word, 1))
